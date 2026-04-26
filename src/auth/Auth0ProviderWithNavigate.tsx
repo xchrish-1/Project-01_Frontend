17e732ef40f2e8311de8a43669ab18e74a0354a5
Chrish Parmar <chrishparmar15604@gmail.com>
2026-04-26 19:10:45 +0530

    debug: log auth0 env variables

--- src/auth/Auth0ProviderWithNavigate.tsx
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -12,7 +12,8 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
   const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
-
+  
+  console.log({ domain, clientId, redirectUri, audience });
   if (!domain || !clientId || !redirectUri || !audience) {
     throw new Error("unable to initialise auth");
   }
