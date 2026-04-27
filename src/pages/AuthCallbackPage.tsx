0b8a8355a6bfac96bb37eedd24bfb9a1815a2c04
Chrish Parmar <chrishparmar15604@gmail.com>
2026-04-27 16:25:00 +0530

    fix: navigate on auth callback error

--- src/pages/AuthCallbackPage.tsx
+++ src/pages/AuthCallbackPage.tsx
@@ -1,3 +1,8 @@
+import { useCreateMyUser } from "@/api/MyUserApi";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+
 const AuthCallbackPage = () => {
   const navigate = useNavigate();
   const { user } = useAuth0();
@@ -20,3 +25,4 @@ const AuthCallbackPage = () => {
 
   return <>Loading...</>;
 };
+export default AuthCallbackPage;  
